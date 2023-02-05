6a4760b6a0841decfb5b5831dbfbf2516f19bf14
d <d@ds-MacBook-Pro.local>
2023-02-04 22:10:54 -0600

    change task background to red

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -24,6 +24,7 @@
         placeholder="Input title"
         style="background: red"
       />
+      />
     </label>
     <button
       v-if="!isChecked"
@@ -36,3 +37,48 @@
     </button>
   </div>
 </template>
+
+<script>
+import { reactive, computed } from "vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Task",
+  props: {
+    task: {
+      type: Object,
+      required: true,
+      default: () => ({ id: "", state: "", title: "" }),
+      validator: (task) => ["id", "state", "title"].every((key) => key in task),
+    },
+  },
+  emits: ["archive-task", "pin-task"],
+
+  setup(props, { emit }) {
+    props = reactive(props);
+    return {
+      classes: computed(() => ({
+        "list-item TASK_INBOX": props.task.state === "TASK_INBOX",
+        "list-item TASK_PINNED": props.task.state === "TASK_PINNED",
+        "list-item TASK_ARCHIVED": props.task.state === "TASK_ARCHIVED",
+      })),
+      /**
+       * Computed property for checking the state of the task
+       */
+      isChecked: computed(() => props.task.state === "TASK_ARCHIVED"),
+      /**
+       * Event handler for archiving tasks
+       */
+      archiveTask() {
+        emit("archive-task", props.task.id);
+      },
+      /**
+       * Event handler for pinning tasks
+       */
+      pinTask() {
+        emit("pin-task", props.task.id);
+      },
+    };
+  },
+};
+</script>
