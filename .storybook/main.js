d159f202da873fb0f29aaded0ff4aae76bddb387
mdevlin14 <mdevlin14@gmail.com>
2023-02-06 08:27:09 -0600

    added external sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,25 @@
 module.exports = {
   stories: ["../src/components/**/*.stories.js"],
+
+  // .storybook/main.js
+
+  refs: {
+    'design-system': {
+      title: 'FORGE Design System',
+      // 👇 The url provided by Chromatic when it was deployed
+      url: 'https://63df23909a5b3a47ad4d2add-dvkinxjybb.chromatic.com/?path=/story/pureinboxscreen--default'
+    }
+  },
+
   staticDirs: ["../public"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    "@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y",
   ],
   framework: "@storybook/vue3",
   core: {
-    builder: "@storybook/builder-webpack5",
+    builder: "@storybook/builder-webpack5"
   },
   features: {
-    interactionsDebugger: true,
-  },
+    interactionsDebugger: true
+  }
 };
